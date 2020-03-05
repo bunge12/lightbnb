418a66d05557becaa3735de2c768ad4e6208318c
Artur Iatsko <arturyatsko@gmail.com>
2020-03-05 23:31:34 +0000

    DB file refactored

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,14 +1,5 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
 
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
+const pool = require('../db/index')
 
 /// Users
 
