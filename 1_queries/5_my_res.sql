264a12092f9622ac89cbf22fd20c49282d2de694
Artur Iatsko <arturyatsko@gmail.com>
2020-03-05 19:46:02 +0000

    Get All Reservations added

--- 1_queries/5_my_res.sql
+++ 1_queries/5_my_res.sql
@@ -3,7 +3,6 @@ FROM reservations
   JOIN properties ON properties.id = reservations.property_id
   JOIN property_reviews ON property_reviews.property_id = reservations.property_id
 WHERE reservations.guest_id = 1
-GROUP BY reservations.id, properties.title, cost_per_night
-HAVING (end_date < now()::date)
+GROUP BY reservations.id, properties.id, properties.title, cost_per_night
 ORDER BY start_date DESC
 LIMIT 10;
