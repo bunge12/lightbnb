c8f36fb20cf481fa243e4c7b7e6fe45e9154a3b7
Artur Iatsko <arturyatsko@gmail.com>
2020-03-05 01:50:49 +0000

    Web App Added

--- 1_queries/5_my_res.sql
+++ 1_queries/5_my_res.sql
@@ -1,4 +1,4 @@
-SELECT reservations.*, properties.*, avg(rating)
+SELECT reservations.*, properties.*, avg(rating) as average_rating
 FROM reservations
   JOIN properties ON properties.id = reservations.property_id
   JOIN property_reviews ON property_reviews.property_id = reservations.property_id
